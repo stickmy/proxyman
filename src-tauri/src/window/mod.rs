fc3016f92b7696c658fed6b14a85150d1fd785c0
stickmy <stickmyc@gmail.com>
2024-05-15 03:09:37 +0800

    refactor: remove initial_window

--- src-tauri/src/window/mod.rs
+++ src-tauri/src/window/mod.rs
@@ -1,33 +1,24 @@
 use std::process::Command;
 
-use tauri::{CustomMenuItem, LogicalSize, Menu, Size, Submenu, Window, WindowMenuEvent};
+use tauri::{CustomMenuItem, Menu, Submenu, WindowMenuEvent};
 
 use crate::app_conf::{self};
 
-pub fn initial_window(win: Window) -> () {
-    win.set_size(Size::Logical(LogicalSize {
-        width: 1200.0,
-        height: 800.0,
-    }))
-    .unwrap();
-
-    win.center().unwrap();
-}
-
-pub fn initial_menu() -> Menu {
-    Menu::new()
-        .add_submenu(Submenu::new("proxyman", Menu::new()))
-        .add_submenu(Submenu::new(
-            "帮助",
-            Menu::new().add_item(CustomMenuItem::new("openlog", "打开日志文件")),
-        ))
+pub fn initial_menu(menu: Menu) -> Menu {
+    menu.add_submenu(Submenu::new(
+        "Help",
+        Menu::new().add_item(CustomMenuItem::new("openlog", "Open Logs")),
+    ))
 }
 
 pub fn register_menu_events(event: WindowMenuEvent) {
     match event.menu_item_id() {
         "openlog" => {
-            Command::new("open").arg(app_conf::app_dir().as_os_str()).output().unwrap();
-        },
+            Command::new("open")
+                .arg(app_conf::app_dir().as_os_str())
+                .output()
+                .unwrap();
+        }
         _ => {}
     };
 }
