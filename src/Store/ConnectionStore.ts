cccfe7778dc83b9f4a4dc445dbbb672a4b675ffd
stickmy <stickmyc@gmail.com>
2024-07-31 00:34:24 +0800

    perf: set connections state per 200ms

--- src/Store/ConnectionStore.ts
+++ src/Store/ConnectionStore.ts
@@ -6,7 +6,6 @@ import {
   isResponseEvent,
 } from "@/Events/ConnectionEvents";
 import { listen } from "@tauri-apps/api/event";
-import { unstable_batchedUpdates } from "react-dom";
 import { create } from "zustand";
 
 export interface Connection {
@@ -18,69 +17,75 @@ export interface Connection {
 export const useConnectionStore = create<{
   connections: Connection[];
   clearConnections: () => void;
-}>(set => ({
+}>((set) => ({
   connections: [] as Connection[],
   clearConnections: () => set({ connections: [] }),
 }));
 
+let cacheConnQueue: RequestConnection[] = [];
+const UPDATE_INTERVAL: number = 200;
+
+let updateTimer: number | undefined = undefined;
+const startUpdateConnStateLoop = () => {
+  updateTimer = window.setTimeout(() => {
+    if (cacheConnQueue.length) {
+      const reqs = cacheConnQueue.map<Connection>((x) => ({
+        id: x.id,
+        request: x,
+      }));
+      useConnectionStore.setState(({ connections }) => ({
+        connections: [...reqs, ...connections],
+      }));
+
+      cacheConnQueue = [];
+    }
+
+    startUpdateConnStateLoop();
+  }, UPDATE_INTERVAL);
+};
+
+startUpdateConnStateLoop();
+
 const processConnections = (event: ConnectionEvent) => {
   if (isRequestEvent(event)) {
-    const reqConn = event.NewRequest;
-
-    // TODO: perf optimization
-    unstable_batchedUpdates(() =>
-      useConnectionStore.setState(({ connections }) => {
-        if (connections.find(x => x.id === reqConn.id)) {
-          console.warn("Duplicate connection:", reqConn.uri);
-        }
-
-        const conn: Connection = {
-          id: reqConn.id,
-          request: reqConn,
-        };
-
-        return { connections: [...connections, conn] };
-      }),
-    );
+    cacheConnQueue.push(event.NewRequest);
   } else if (isResponseEvent(event)) {
     const resConn = event.NewResponse;
 
-    unstable_batchedUpdates(() => {
-      useConnectionStore.setState(({ connections }) => {
-        const conn = connections.find(x => x.id === resConn.id);
+    useConnectionStore.setState(({ connections }) => {
+      const conn = connections.find((x) => x.id === resConn.id);
 
-        if (!conn) {
-          console.warn(
-            "Orphan response(None matched request connection found)",
-          );
-          return { connections: connections };
-        }
+      if (!conn) {
+        console.warn(
+          `Orphan response(None matched request connection found: ${resConn.id})`,
+        );
+        return { connections };
+      }
 
-        const updatedConn: Connection = {
-          id: conn.id,
-          request: conn.request,
-          response: resConn,
-        };
+      const updatedConn: Connection = {
+        id: conn.id,
+        request: conn.request,
+        response: resConn,
+      };
 
-        return {
-          connections: connections.map(x =>
-            x.id === conn.id ? updatedConn : x,
-          ),
-        };
-      });
+      return {
+        connections: connections.map((x) =>
+          x.id === conn.id ? updatedConn : x,
+        ),
+      };
     });
   }
 };
 
 // @ts-ignore
-const unlisten = listen<ConnectionEvent>("proxy_event", event => {
+const unlisten = listen<ConnectionEvent>("proxy_event", (event) => {
   const payload = event.payload;
   console.debug("connections", payload);
   processConnections(payload);
 });
 
 window.addEventListener("beforeunload", () => {
-  unlisten.then(f => f());
+  unlisten.then((f) => f());
 });
 
 // @ts-ignore
